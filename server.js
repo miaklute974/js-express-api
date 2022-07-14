9072da01cd48c7facb77c1c4ea955c54297b816b
Eric Steele <steelederic@gmail.com>
2022-07-14 14:29:12 -0400

    added functions to call shortcut endpoints for story, owner and epic. added sendToSheets func to simulate post to external API

--- server.js
+++ server.js
@@ -25,28 +25,101 @@ app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
 // Routes
 app.get('/', (req, res) => {
-    //res.status(200).send('Docker');
-    res.json({ "foo": "bar" });
+    res.json({ "sanity": "check" });
 });
 
-
-app.post('/webhook', (req, res) => {
-    //TODO: Parse data, grab what we need from shortcut, send it to google sheets API.
+//handle webhook from shortcut, call function to send to sheets
+app.post('/webhook',  async (req, res) => {
     var body = req.body;
+    await sendToSheets(body.actions[0].id)
     res.send(body);
-
-    // Need to check for : "story_type", as well as if the story is still completed after a certain amount of time. Background request after x seconds/minutes?
-    axios.post('https://webhook.site/fab5af00-2101-4a55-92e6-d73b79b3d1a5', JSON.stringify(body.actions[0].id)) // this seems stupid but axios bitched
-        .then((res) => {
-            console.log(`Status: ${res.status}`);
-            console.log('Body: ', res.data);
-        }).catch((err) => {
-            console.error(err);
-        });
-
 });
 
 
+async function getStory(id) {
+
+    const config = {
+        method: 'get',
+        url: `https://api.app.shortcut.com/api/v3/stories/${id}`,
+        headers: HEADERS
+    }
+
+    let res = await axios(config)
+
+    owner = await getOwner(res.data.owner_ids[0])
+    requester = await getOwner(res.data.requested_by_id)
+    epic = await getEpic(res.data.epic_id)
+
+    let story = {
+        'story_id': res.data.id,
+        'story_name': res.data.name,
+        'story_type': res.data.story_type,
+        'requester_id': requester.name,
+        'owners': owner.name,
+        'is_completed': res.data.completed,
+        'external_tickets': res.data.external_links,
+        'epic_id': res.data.epic_id,
+        'epic_name': epic.name
+    }
+
+    //console.log(story);
+    return story
+
+};
+
+// TODO: make this filter on a specific ID. I believe this returns a list of owners. Defaulting to the first index of the JSON within getStory() currently.
+async function getOwner() {
+
+    const config = {
+        method: 'get',
+        url: `https://api.app.shortcut.com/api/v3/member`,
+        headers: HEADERS
+    }
+
+    let res = await axios(config)
+
+    return { 'name': res.data.name }
+};
+
+
+async function getEpic(id) {
+
+    const config = {
+        method: 'get',
+        url: `https://api.app.shortcut.com/api/v3/epics/${id}`,
+        headers: HEADERS
+    }
+
+    let res = await axios(config)
+
+    return { 'name': res.data.name }
+};
+
+//simulating sending to sheets
+async function sendToSheets(id) {
+    await getStory(id).then((res) => {
+        sheets = axios.post('https://webhook.site/9f6005cd-4464-4140-a1a9-aa3c391d173b', JSON.stringify(res)) // this seems stupid but axios bitched
+            .then((res) => {
+                console.log(`Status: ${res.status}`);
+            }).catch((err) => {
+                console.error(err);
+            });
+    });
+    
+
+    // console.log(story);
+};
+
+
+
+
+
+// story =  getStory(13)
+// console.log(story)
+// getEpic(12)
+
+
+//https://api.app.shortcut.com/api/v3/stories/{story-public-id}
 
 app.listen(config.port, config.host, (e) => {
     if (e) {
