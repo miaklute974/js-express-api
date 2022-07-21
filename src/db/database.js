09fe11d24ee0fcb92d1d58efd0195e84a2f02134
Eric Steele <steelederic@gmail.com>
2022-07-21 15:03:57 -0400

    refactor routes in server.js -> /routes/google. Changed database.js connection details into .env

--- src/db/database.js
+++ src/db/database.js
@@ -4,12 +4,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
-                                process.env.DB_USER || 'postgres',
-                                process.env.DB_PASSWORD || '',
+const sequelize = new Sequelize(process.env.DB_SCHEMA,
+                                process.env.DB_USER,
+                                process.env.DB_PASSWORD,
                                 {
-                                    host: process.env.DB_HOST || 'localhost',
-                                    port: process.env.DB_PORT || 5432,
+                                    host: process.env.DB_HOST,
+                                    port: process.env.DB_PORT,
                                     dialect: 'postgres',
                                     dialectOptions: {
                                         ssl: process.env.DB_SSL == "false"
--- src/server.js
+++ src/server.js
@@ -6,11 +6,8 @@ const bodyParser = require('body-parser');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
 const webhook = require('./routes/webhook');
 const storiesRouter = require('./routes/stories');
+const googleRouter = require('./routes/google');
 const oAuthTokensRouter = require('./routes/oauth_tokens');
-const url = require("./google/google-oauth-generate-url")
-const token = require("./google/google-oauth-get-tokens")
-const sheet = require("./google/google-sheet")
-
 
 
 const config = {
@@ -18,7 +15,6 @@ const config = {
     port: 3000,
     host: '0.0.0.0',
 };
-
 
 const app = express();
 const logger = log({ console: true, file: false, label: config.name });
@@ -29,40 +25,7 @@ app.use(ExpressAPILogMiddleware(logger, { request: true }));
 app.use(cors())
 app.use('/stories', storiesRouter)
 app.use('/oauth_tokens', oAuthTokensRouter)
-
-//grab code from sign in to oauth link
-app.get('/auth', async (req, res) => {
-    auth_url = await url.generateUrl()
-    res.redirect(auth_url)
-
-});
-
-
-// I should probably create a route to generate a token, otherwise we are generating the token over and over...
-app.get('/', async (req, res) => {
-    var code = req.query.code;
-    console.log(code)
-
-    // oauth_token = await token.getToken(code)
-    try {
-        await sheet.writeGoogleSheet()
-    } catch (error) {
-        console.log(error);
-        await token.getToken(code)
-
-        console.log('Trying again...')
-        await sheet.writeGoogleSheet()
-    }
-
-    res.redirect("/success")
-
-});
-
-
-//test if we were successful
-app.get('/success', async (req, res) => {
-    res.json({ "Auth": "Success" });
-});
+app.use('/', googleRouter)
 
 
 app.listen(config.port, config.host, (e) => {
