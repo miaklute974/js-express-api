f40654e4c00429126eda9e1c9d94a750fea7c2ec
Eric Steele <steelederic@gmail.com>
2022-07-22 13:02:39 -0400

    npm sqlite/database.js -> sqlite::memory

--- src/db/database.js
+++ src/db/database.js
@@ -4,17 +4,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_SCHEMA,
-                                process.env.DB_USER,
-                                process.env.DB_PASSWORD,
-                                {
-                                    host: process.env.DB_HOST,
-                                    port: process.env.DB_PORT,
-                                    dialect: 'postgres',
-                                    dialectOptions: { 
-                                        ssl: false //process.env.DB_SSL //== "false"
-                                    }
-                                });
+const sequelize = new Sequelize("sqlite::memory:");
 
 // TODO: create models dir, import sequelize as db where needed.                               
 const Story = sequelize.define('story', {
